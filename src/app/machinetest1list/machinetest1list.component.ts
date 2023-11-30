c3b7c7a527229111d571d27636e5d72c93c44bc1
Rushikesh Bairagi <122372762+Rushikeshbairagi9922@users.noreply.github.com>
2023-11-30 14:19:05 +0530

    First commit

--- src/app/machinetest1list/machinetest1list.component.ts
+++ src/app/machinetest1list/machinetest1list.component.ts
@@ -8,63 +8,60 @@ import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-machinetest1list',
   templateUrl: './machinetest1list.component.html',
-  styleUrls: ['./machinetest1list.component.css']
+  styleUrls: ['./machinetest1list.component.css'],
 })
 export class Machinetest1listComponent implements OnInit {
-
-  product : any;
-  page: number = 1; 
-  isOnline: boolean= true;
+  product: any;
+  page: number = 1;
+  isOnline: boolean = true;
   darkMode = false;
-  form !:FormGroup;
+  form!: FormGroup;
   searchTerm: string = '';
-  
 
-constructor(public rout: Router,public _httpdemo: HttpClientDemoService,  private networkcheck :NetworkcheckService ){
-  
-}
+  constructor(
+    public rout: Router,
+    public _httpdemo: HttpClientDemoService,
+    private networkcheck: NetworkcheckService
+  ) {}
 
-ngOnInit(): void {
-  this.networkcheck.connectionStatus$.subscribe((isOnline)=>{
-    this.isOnline = isOnline;
-  })
-
-  this._httpdemo.getMachinetest().subscribe((e:any)=>this.product=e.products);
-  
-  console.log("prolist"+this.product);
-
-  //darkmode
-  this.form = new FormGroup({
-    darkMode: new FormGroup(this.darkMode)
-  });
-  
-}
-logout(){
-  localStorage.removeItem('userData');
-  this.rout.navigate(['MachineLogin']);
-}
-
-
-
-toggleDarkMode() {
-  this.darkMode = !this.darkMode;
-  this.form.patchValue({ darkMode: this.darkMode });
-}
-
-
-
-get filteredProducts() {
-  if (!this.searchTerm.trim()) {
-    return this.product;
-  }
-// console.log(this.product)
-  return this.product.filter((p: any) => {
-    return Object.values(p).some((value: any) => {
-      if (typeof value === 'string') {
-        return value.toLowerCase().includes(this.searchTerm.toLowerCase());
-      }
-      return false;
+  ngOnInit(): void {
+    this.networkcheck.connectionStatus$.subscribe((isOnline) => {
+      this.isOnline = isOnline;
     });
-  });
-}
+
+    this._httpdemo
+      .getMachinetest()
+      .subscribe((e: any) => (this.product = e.products));
+
+    console.log('prolist' + this.product);
+
+    //darkmode
+    this.form = new FormGroup({
+      darkMode: new FormGroup(this.darkMode),
+    });
+  }
+  logout() {
+    localStorage.removeItem('userData');
+    this.rout.navigate(['MachineLogin']);
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    this.form.patchValue({ darkMode: this.darkMode });
+  }
+
+  get filteredProducts() {
+    if (!this.searchTerm.trim()) {
+      return this.product;
+    }
+    // console.log(this.product)
+    return this.product.filter((p: any) => {
+      return Object.values(p).some((value: any) => {
+        if (typeof value === 'string') {
+          return value.toLowerCase().includes(this.searchTerm.toLowerCase());
+        }
+        return false;
+      });
+    });
+  }
 }
